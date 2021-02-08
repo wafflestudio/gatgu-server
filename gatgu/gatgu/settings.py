88b004294055de9d1b3be26f5e09227f4ae259eb
Noaarhk <skarb8277@naver.com>
2021-02-08 21:20:01 +0900

    add pylint & fix typo

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    # 'rest_framework.authtoken'
+    'rest_framework.authtoken',
 
     'user.apps.UserConfig',
 
