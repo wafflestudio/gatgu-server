21d47b120fd9ce05156dce7a2d9a2ba713053dc5
Unamq <69029281+Noaarhk@users.noreply.github.com>
2021-02-15 14:41:36 +0900

    Revert "User/#21 (#23)" (#26)
    This reverts commit f1b33de384dce5917a9481a1de08069d03bb7d60.

--- gatgu/user/models.py
+++ gatgu/user/models.py
@@ -2,15 +2,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(
-        User, related_name='userprofile', on_delete=models.CASCADE)
-    picture = models.ImageField(default='default.jpg')
-    nickname = models.CharField(
-        max_length=10, db_index=True, blank=True, unique=True)
-    address = models.CharField(max_length=20, blank=True)
-    phonenumber = models.CharField(
-        max_length=13, db_index=True, blank=True, unique=True)
-    is_snu = models.BooleanField(default=False)
-    updated_at = models.DateTimeField(auto_now=True)
-    withdrew_at = models.DateTimeField(null=True)
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
+    area = models.CharField(max_length=20, blank=True)
+    nickname = models.CharField(max_length=10, db_index=True, blank=True, unique=True)
+    phone = models.CharField(max_length=13, db_index=True, blank=True, unique=True)
+    USER_TYPE = (
+        (1, 'django'),
+        (2, 'kakao'),
+    )
+
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE, default=1, null=True)
+    profile_pics = models.ImageField(default='default.jpg')
+
