9a699bd6ac283118129a8de9d6ba65fc1b3cc756
cs71107 <69337848+cs71107@users.noreply.github.com>
2021-02-18 17:26:57 +0900

    user api update, bug resolve, django-debug-toolbar setting. (#28)
    * user api update, bug resolve, django-debug-toolbar setting.
    
    * api param 추가, test_user
    
    * pylint+line
    
    * wrong phonenumber testcase erase
    
    * phonenumber -> phone
    
    * transaction add
    
    * import transaction
    
    * empty nickname, phone issue resolve

--- gatgu/user/serializers.py
+++ gatgu/user/serializers.py
@@ -5,48 +5,49 @@ from django.db import transaction
 from rest_framework import serializers, status
 from rest_framework.authtoken.models import Token
 from user.models import UserProfile
+import datetime
 
 
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
-    email = serializers.EmailField(allow_blank=False)
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(allow_blank=False)
     first_name = serializers.CharField(required=False)
     last_name = serializers.CharField(required=False)
     last_login = serializers.DateTimeField(read_only=True)
-    joined_at = serializers.DateTimeField(read_only=True)
+    date_joined = serializers.DateTimeField(read_only=True)
     userprofile = serializers.SerializerMethodField()
-    user_type = serializers.ChoiceField(write_only=True, allow_null=True, required=False, choices=UserProfile.USER_TYPE)
-    area = serializers.CharField(write_only=True, allow_blank=False, required=False)
-    nickname = serializers.CharField(write_only=True, allow_blank=False, required=False)
-    phone = serializers.CharField(write_only=True,
-                                  allow_blank=False,
-                                  max_length=13,
-                                  required=False,
-                                  validators=[RegexValidator(regex=r'^[0-9]{3}-([0-9]{3}|[0-9]{4})-[0-9]{4}$',
-                                                             message="Phone number must be entered in the format '000-0000-0000'",
-                                                             )
-                                              ]
-                                  )
-    profile_pics = serializers.ImageField(write_only=True, required=False, allow_null=True, use_url=True)
+    is_active = serializers.BooleanField(default=True)
+    address = serializers.CharField(
+        write_only=True, allow_blank=False, required=False)
+    nickname = serializers.CharField(
+        write_only=True, allow_blank=False, required=False)
+    phone = serializers.CharField(
+        write_only=True,
+        allow_blank=False,
+        max_length=13,
+        required=False,
+    )
+    picture = serializers.ImageField(
+        write_only=True, required=False, allow_null=True, use_url=True)
 
     class Meta:
         model = User
         fields = (
             'id',
             'username',
-            'user_type',
-            'email',
             'password',
+            'email',
             'first_name',
             'last_name',
+            'date_joined',
             'last_login',
-            'joined_at',
             'userprofile',
-            'area',
+            'is_active',
+            'address',
             'nickname',
             'phone',
-            'profile_pics',
+            'picture',
         )
 
     def get_userprofile(self, user):
@@ -61,63 +62,73 @@ class UserSerializer(serializers.ModelSerializer):
         last_name = data.get('last_name')
 
         if bool(first_name) ^ bool(last_name):
-            api_exception = serializers.ValidationError("First name and last name should appear together.")
+            message = "First name and last name should appear together."
+            api_exception = serializers.ValidationError(message)
             api_exception.status_code = status.HTTP_400_BAD_REQUEST
             raise api_exception
         if first_name and last_name and not (first_name.isalpha() and last_name.isalpha()):
-            api_exception = serializers.ValidationError("First name or last name should not have number.")
+            message = "First name or last name should not have number."
+            api_exception = serializers.ValidationError(message)
             api_exception.status_code = status.HTTP_400_BAD_REQUEST
             raise api_exception
-
-        # profile_serializer = UserProfileSerializer(data=data, context=self.context)
-        # profile_serializer.is_valid(raise_exception=True)
-
         return data
 
     @transaction.atomic
     def create(self, validated_data):
-        validated_data.pop('area', '')
+        validated_data.pop('address', '')
         validated_data.pop('nickname', '')
         validated_data.pop('phone', '')
-        validated_data.pop('user_type', None)
-        validated_data.pop('profile_pics', None)
+        validated_data.pop('picture', None)
         user = super(UserSerializer, self).create(validated_data)
         Token.objects.create(user=user)
 
         return user
 
     def update(self, user, validated_data):
-        area = validated_data.get('area')
+
+        address = validated_data.get('address')
         nickname = validated_data.get('nickname')
         phone = validated_data.get('phone')
-        profile_pics = validated_data.get('profile_pics')
-
-        # user_type = validated_data.pop('user_type', '')
+        picture = validated_data.get('picture')
 
         profile = user.userprofile
-        if area is not None:
-            profile.area = area
+        if address is not None:
+            profile.address = address
         if nickname is not None:
             profile.nickname = nickname
         if phone is not None:
             profile.phone = phone
-        if profile_pics is not None:
-            profile.profile_pics = profile_pics
-        #        if user_type is not None:
-        #            profile.user_type = user_type
+        if picture is not None:
+            profile.picture = picture
+
         profile.save()
 
         return super(UserSerializer, self).update(user, validated_data)
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    address = serializers.CharField(allow_blank=False, required=False)
+    nickname = serializers.CharField(allow_blank=False, required=False)
+    phone = serializers.CharField(
+        allow_blank=False,
+        max_length=13,
+        required=False,
+    )
+    is_snu = serializers.BooleanField(read_only=True, default=False)
+    updated_at = serializers.DateTimeField(read_only=True)
+    withdrew_at = serializers.DateTimeField(read_only=True, allow_null=True)
+    picture = serializers.ImageField(
+        required=False, allow_null=True, use_url=True)
+
     class Meta:
         model = UserProfile
         fields = [
             'id',
-            'user_type',
-            'area',
+            'address',
             'nickname',
             'phone',
-            'profile_pics',
+            'is_snu',
+            'updated_at',
+            'withdrew_at',
+            'picture',
         ]
