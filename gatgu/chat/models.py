aa4e538e07d5121f35927f1318614a2b4a55f911
김기덕 <gideok@gimgideog-ui-MacBookPro.local>
2021-02-21 18:30:52 +0900

    first commit with models

--- gatgu/chat/models.py
+++ gatgu/chat/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from article.models import Article
 # Create your models here.
+
+class OrderChat(models.Model):
+    participants = models.ManyToManyField(
+        User,
+        through='ParticipantProfile',
+        through_fields=('order_id', 'participant_id'),
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    order_status = models.CharField(max_length=30)
+    tracking_number = models.CharField(max_length=30)
+    required_people = models.IntegerField()
+    cur_people = models.IntegerField()
+
+class ChatMessages(models.Model):
+    text = models.CharField(max_length=30)
+    sent_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    sent_at = models.DateField()
+    chatroom_id = models.ForeignKey(
+        OrderChat,
+        on_delete=models.CASCADE
+    )
+    media = models.CharField(max_length=1000)
+    type = models.CharField(max_length=30)
+
+class ParticipantProfile(models.Model):
+    order_id = models.ForeignKey(
+        OrderChat, 
+        on_delete=models.CASCADE
+    )
+    participant_id = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE
+    )
+    joined_at = models.DateField()
+    out_at = models.DateField()
+    pay_status = models.CharField(max_length=30)
--- gatgu/chat/views.py
+++ gatgu/chat/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from chat.models import OrderChat, ChatMessages, ParticipantProfile
+from django.contrib.auth.models import User
 # Create your views here.
+
+# /chat/<int:chat_id>/
+@csrf_exempt
+def chat(request, chat_id):
+    if request.method == 'GET':
+        chat = OrderChat.objects.get(id=chat_id)
+        participants = chat.participants
+        response = {'chat_id': chat_id, 'article_id': article_id, 'participants': participants, 'order_status': chat.order_status, 'tracking_number': chat.tracking_number}
+        return JsonResponse(response, safe=False, status=200)
+
+# /chat/join/<int:chat_id>/
+def join(request, chat_id):
+    if request.method == 'PUT': # already in => success
+        chat = OrderChat.objects.get(id=chat_id)
+        response = {'status': 'success'}
+        user = User.objects.get(id=request.user_id)
+
+        if user in chat.participants:
+            pass 
+        elif chat.cur_people < chat.required_people: # can go in => success
+
+            chat.cur_people += 1
+            pass 
+        else:
+            response.status = 'failure' # full room => failure
+
+        return JsonResponse(response, safe=False, status=200)
+
+# /chat/out/<int:chat_id>/
+def out(request, chat_id):
+    if request.method == 'PUT':
+        chat = OrderChat.objects.get(id=chat_id)
+        response = {'status': 'success'}
+        user = User.objects.get(id=request.user_id)
+
+        if user not in chat.participants: # not in room
+            response.status = 'failure'
+        else: # room member is going out
+            pass
+        
+        return JsonResponse(response, safe=False, status=200)
+        
+# /chat/<int:chat_id>/message/
+def messages(request, chat_id):
+    if request.method == 'GET':
+        messages = [message for message in ChatMessage.objects.filter(chatroom_id==chat_id).values()]
+        user = request.user
+        joined_at = ParticipantProfile.objects.get(order_id==chat_id, participant_id==user.id)
+        messages = messages.filter(joined_at<sent_at)
+        
+        return JsonResponse(messages, safe=False, status=200)
+    elif request.method == 'POST':
+        
+# /chat/message/<int:message_id>/
+def message(request, message_id):
+    if request.method == 'GET':
+# /chat/<int:chat_id>/participants/
+def participants(request, chat_id):
+    pass
