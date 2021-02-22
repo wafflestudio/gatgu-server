65ec37fc084d8ed955d970678c07e468ebf429e7
cs71107 <psjlds@gmail.com>
2021-02-22 13:44:39 +0900

    settings update (email)

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -121,6 +121,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email
+
+EMAIL_HOST = 'smtp.gmail.com'
+# 메일을 호스트하는 서버
+EMAIL_PORT = '587'
+# gmail과의 통신하는 포트
+EMAIL_HOST_USER = 'cs71107@wafflestudio.com'
+# 발신할 이메일
+EMAIL_HOST_PASSWORD = 'psj8904021$'
+# 발신할 메일의 비밀번호
+EMAIL_USE_TLS = True
+# TLS 보안 방법
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# 사이트와 관련한 자동응답을 받을 이메일 주소,'webmaster@localhost'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
