e3fe4d79b96a5874631bbabd294a44eed1026222
Noaarhk <skarb8277@naver.com>
2021-02-26 09:28:38 +0900

    Set RDS

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'gatgu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'my-database.c8rxsbbexj0l.us-east-2.rds.amazonaws.com',
+        'HOST': 'gatgu-database.c8rxsbbexj0l.us-east-2.rds.amazonaws.com',
         'PORT': 3306,
         'NAME': 'gatgu_db',
         'USER': 'team-gatgu',
