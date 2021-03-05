9288d9e651841ea9a4143f6b710209042ad13622
Unamq <69029281+Noaarhk@users.noreply.github.com>
2021-03-05 22:41:05 +0900

    [Re]Cur vals/#48 (#54)
    * build structure
    
    * add user api
    
    * article api in progress
    
    * Set article model field
    
    * pull merge
    
    * pull merge
    
    * create Article
    
    * Fix some on User api & Write article api
    
    * first commit with models
    
    * CRUD of article
    
    * Delete secrets.json
    
    * delete secrets
    
    * delete fields 'is_deleted'
    
    * revise need_type & initialize migration files
    
    * Delete 0001_initial.py
    
    * Delete 0002_auto_20210219_0712.py
    
    * Delete 0003_auto_20210219_1014.py
    
    * Delete 0004_auto_20210219_1105.py
    
    * Delete 0005_article_is_deleted.py
    
    * Recreate  migration files
    
    * view and consumer
    
    * retrieve user's hosted list
    
    * Try pagination
    
    * add view invalid request
    
    * test
    
    * add api error code
    
    * revise return form of message
    
    * test 401
    
    * add channels on pip list
    
    * fix error
    
    * paginator test
    
    * pagination commit
    
    * Adding current price & aggregate
    
    * Add page_size
    
    * Revise model
    
    * migrations merge
    
    * Reflect Feedback
    
    * Reflect Feedback
    
    * stash merge
    
    * stash merge
    
    * Trim model & Create OrderChat
    
    * Add chat, participants info on ArticleSerializer
    
    * Return Current values_V2
    
    * Article plus/#37 (#47)
    
    * build structure
    
    * add user api
    
    * article api in progress
    
    * Set article model field
    
    * pull merge
    
    * pull merge
    
    * create Article
    
    * Fix some on User api & Write article api
    
    * CRUD of article
    
    * Delete secrets.json
    
    * delete secrets
    
    * delete fields 'is_deleted'
    
    * revise need_type & initialize migration files
    
    * Delete 0001_initial.py
    
    * Delete 0002_auto_20210219_0712.py
    
    * Delete 0003_auto_20210219_1014.py
    
    * Delete 0004_auto_20210219_1105.py
    
    * Delete 0005_article_is_deleted.py
    
    * Recreate  migration files
    
    * retrieve user's hosted list
    
    * Try pagination
    
    * fix error
    
    * paginator test
    
    * pagination commit
    
    * Add page_size
    
    * Reflect Feedback
    
    * User/#35 (#39)
    
    * user update
    
    * settings update (email)
    
    * email certification first commit (not completed)
    
    * email certification update
    
    * erase duplicate
    
    * migrations merge and create reconfirm.
    
    * fix typos.
    
    * cache update
    
    * caches update, accessing limit.
    
    Co-authored-by: Unamq <69029281+Noaarhk@users.noreply.github.com>
    
    * Clear lines
    
    * Clear lines
    
    * Convert Image Field -> UrlField
    
    Co-authored-by: 김기덕 <gideok@gimgideog-ui-MacBookPro.local>
    Co-authored-by: cs71107 <69337848+cs71107@users.noreply.github.com>

--- gatgu/chat/models.py
+++ gatgu/chat/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
+from article.models import Article
 
-# Create your models here.
+
+class OrderChat(models.Model):
+    article = models.OneToOneField(
+        Article,
+        on_delete=models.CASCADE,
+        related_name='order_chat'
+    )
+
+    ORDER_STATUS = (
+        (1, 'WAITING_MEMBERS'),
+        (2, 'MEMBER_ASSEMBLED'),
+        (3, 'PAY_STATUS_CHECKED'),
+        (4, 'ORDER_COMPLETE'),
+        (5, 'WAITING_PARCELS'),
+        (6, 'WAITING_SHARE'),
+        (7, 'GATGU_COMPLETE'),
+    )
+
+    order_status = models.PositiveSmallIntegerField(choices=ORDER_STATUS, default=1, null=True)
+
+    tracking_number = models.CharField(max_length=30, null=True)
+
+    '''participants.count로 대체'''
+    # cur_people = models.IntegerField()
+
+
+class ChatMessage(models.Model):
+    text = models.TextField(null=True)
+    sent_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    sent_at = models.DateTimeField(auto_now=True)
+    chat = models.ForeignKey(
+        OrderChat,
+        on_delete=models.CASCADE,
+        related_name='messages'
+    )
+    media = models.URLField(null=True)
+    type = models.CharField(max_length=30)
+
+
+class ParticipantProfile(models.Model):
+    order = models.ForeignKey(
+        OrderChat,
+        on_delete=models.CASCADE,
+        related_name='participant_profile',
+    )
+    participant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='participant_profile',
+    )
+    joined_at = models.DateTimeField(auto_now=True)
+    out_at = models.DateTimeField(null=True)
+    pay_status = models.BooleanField(default=False)
+    wish_price = models.IntegerField(null=True)
+
+    class Meta:
+        unique_together = (
+            ('order', 'participant')
+        )
