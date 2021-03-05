bdcf3333427151df4fb261086697a3a7fb0d574d
cs71107 <69337848+cs71107@users.noreply.github.com>
2021-03-05 09:41:16 +0900

    User/#35 (#39)
    * user update
    
    * settings update (email)
    
    * email certification first commit (not completed)
    
    * email certification update
    
    * erase duplicate
    
    * migrations merge and create reconfirm.
    
    * fix typos.
    
    * cache update
    
    * caches update, accessing limit.
    
    Co-authored-by: Unamq <69029281+Noaarhk@users.noreply.github.com>

--- gatgu/user/serializers.py
+++ gatgu/user/serializers.py
@@ -121,17 +121,15 @@ class UserProfileSerializer(serializers.ModelSerializer):
         use_url=True,
         required=False,
     )
-    is_snu = serializers.BooleanField(read_only=True, default=False)
     updated_at = serializers.DateTimeField(read_only=True)
     withdrew_at = serializers.DateTimeField(read_only=True, allow_null=True)
 
     class Meta:
         model = UserProfile
-        fields = [
+        fields = (
             'id',
             'nickname',
             'picture',
-            'is_snu',
             'updated_at',
             'withdrew_at',
             'picture',
