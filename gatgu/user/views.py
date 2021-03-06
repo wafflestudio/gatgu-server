549c093d719845a9791c8d2c224d9c5f8521dbf7
cs71107 <psjlds@gmail.com>
2021-03-06 15:58:27 +0900

    user error to hanguel

--- gatgu/user/views.py
+++ gatgu/user/views.py
@@ -60,14 +60,14 @@ class UserViewSet(viewsets.GenericViewSet):
 
         if not username or not password or not email:
             response_data = {
-                "error": "username, password, email are required."}
+                "error": "아이디, 비밀번호, 이메일은 필수 항목입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         if not EmailProfile.objects.filter(
                 email=email,
                 is_pending=True).exists():
             response_data = {
-                "error": "uncertificated email. please certificate email"}
+                "error": "인증되지 않은 이메일입니다. 인증을 해주세요."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         email_profile = EmailProfile.objects.filter(
@@ -78,13 +78,13 @@ class UserViewSet(viewsets.GenericViewSet):
 
         if not nickname:
             response_data = {
-                "error": "nickname are required."}
+                "error": "닉네임은 필수 항목입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         if UserProfile.objects.filter(nickname__iexact=nickname,
                                       withdrew_at__isnull=True).exists():  # only active user couldn't conflict.
             response_data = {
-                "error": "A user with that Nickname already exists."}
+                "error": "해당 닉네임은 사용할 수 없습니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = self.get_serializer(data=data)
@@ -97,7 +97,7 @@ class UserViewSet(viewsets.GenericViewSet):
             user = serializer.save()
         except IntegrityError:
             response_data = {
-                "error": "A user with that username already exists."}
+                "error": "해당 아이디는 사용할 수 없습니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         email_profile.is_pending = False
@@ -126,13 +126,13 @@ class UserViewSet(viewsets.GenericViewSet):
             data['token'] = token.key
             return Response(data)
 
-        response_data = {"error": "Wrong username or wrong password"}
+        response_data = {"error": "아이디나 패스워드가 잘못 됐습니다."}
         return Response(response_data, status=status.HTTP_403_FORBIDDEN)
 
     @action(detail=False, methods=['PUT'])  # 로그아웃
     def logout(self, request):
         logout(request)
-        return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+        return Response({"message": "성공적으로 로그아웃 됐습니다."}, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['PUT'], url_path='confirm', url_name='confirm')
     def confirm(self, request):
@@ -140,7 +140,7 @@ class UserViewSet(viewsets.GenericViewSet):
         email = request.data.get("email")
 
         if EmailProfile.objects.filter(email=email, is_pending=True).exists():
-            response_data = {"This email is already pending now."}
+            response_data = {"error": "이미 인증이 된 이메일입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         ncache = caches["number_of_confirm"]
@@ -151,7 +151,7 @@ class UserViewSet(viewsets.GenericViewSet):
             confirm_number = 0
 
         if confirm_number >= 10:
-            response_data = {"Too many confirming access!"}
+            response_data = {"error": "너무 많이 인증요청을 하셨습니다. 2시간 후에 다시 시도해주십시오."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         code = generate_code()
@@ -164,7 +164,7 @@ class UserViewSet(viewsets.GenericViewSet):
 
         self.send_mail(email, code)
 
-        return Response({"message": "Successfully send confirming email"}, status=status.HTTP_200_OK)
+        return Response({"message": "성공적으로 인증 메일을 발송하였습니다."}, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['PUT'], url_path='activate', url_name='activate')
     def activate(self, request):
@@ -178,18 +178,18 @@ class UserViewSet(viewsets.GenericViewSet):
         email_code = cache.get(email)
 
         if email_code is None:
-            response_data = {"Time is over or no such email."}
+            response_data = {"error": "시간이 초과되었거나, 인증 요청을 하지 않은 이메일입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         if email_code != code:
-            response_data = {"Code is doesn't matching."}
+            response_data = {"error": "코드가 맞지 않습니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         EmailProfile.objects.create(email=email)
         cache.set(email, code, timeout=0)  # erase from cache
         ncache.set(email, 0, timeout=0)
 
-        response_data = {"Successfully activated."}
+        response_data = {"message": "성공적으로 인증하였습니다."}
         return Response(response_data, status=status.HTTP_200_OK)
 
     # Get /user/{user_id} # 유저 정보 가져오기(나 & 남)
@@ -201,14 +201,14 @@ class UserViewSet(viewsets.GenericViewSet):
             try:
                 user = User.objects.get(pk=pk)
             except User.DoesNotExist:
-                response_data = {"message": "There is no such user."}
+                response_data = {"message": "해당하는 사용자가 없습니다."}
                 return Response(response_data, status=status.HTTP_404_NOT_FOUND)
 
         if not request.user.is_superuser:
 
             if not user.is_active or user.is_superuser:
                 response_data = {
-                    "message": "Coudn't get this user's information."}
+                    "message": "다른 유저의 정보를 볼 수 없습니다."}
                 return Response(response_data, status=status.HTTP_403_FORBIDDEN)
             else:
                 pass
@@ -239,17 +239,17 @@ class UserViewSet(viewsets.GenericViewSet):
             user.is_active = False
             user.save()
         else:
-            response_data = {"message": "This user Already withdrew"}
+            response_data = {"error": "이미 탈퇴한 유저입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response({"message": "Successfully deactivated."}, status=status.HTTP_200_OK)
+        return Response({"message": "성공적으로 탈퇴 하였습니다."}, status=status.HTTP_200_OK)
 
     @transaction.atomic
     # PUT /user/me/  # 유저 정보 수정 (나)
     def update(self, request, pk=None):
 
         if pk != 'me':
-            response_data = {"error": "Can't update other Users information"}
+            response_data = {"error": "다른 사용자의 정보를 수정할 수 없습니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         user = request.user
@@ -263,7 +263,7 @@ class UserViewSet(viewsets.GenericViewSet):
                 cnt = cnt + 1
 
         if cnt != len(data):
-            response_data = {"error": "Request has invalid key"}
+            response_data = {"error": "수정할 수 없는 정보가 있는 요청입니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         nickname = data.get('nickname')
@@ -271,7 +271,7 @@ class UserViewSet(viewsets.GenericViewSet):
         if UserProfile.objects.filter(nickname__iexact=nickname,
                                       withdrew_at__isnull=True).exclude(user_id=user.id).exists():
             response_data = {
-                "error": "A user with that Nickname already exists."}
+                "error": "해당 닉네임은 사용할 수 없습니다."}
             return Response(response_data, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = self.get_serializer(user, data=request.data, partial=True)
@@ -286,7 +286,8 @@ class UserViewSet(viewsets.GenericViewSet):
     def hosted_list(self, request, pk):
         user_tar = self.get_object()
         if user_tar.is_active:
-            hosted = Article.objects.filter(deleted_at=None, writer=user_tar).all()
+            hosted = Article.objects.filter(
+                deleted_at=None, writer=user_tar).all()
             if hosted:
                 data = ArticleSerializer(hosted, many=True).data
                 return Response(data, status=status.HTTP_200_OK)
@@ -294,4 +295,3 @@ class UserViewSet(viewsets.GenericViewSet):
                 return Response(' message : article not found ', status=status.HTTP_404_NOT_FOUND)
         else:
             return Response('message : user deactivated ', status=status.HTTP_404_NOT_FOUND)
-
