428869e3dfeae756465bdf531131e6b8bbf55549
cs71107 <psjlds@gmail.com>
2021-03-06 16:43:04 +0900

    admin userprofile add

--- gatgu/user/admin.py
+++ gatgu/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from . import models
 
-# Register your models here.
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user_id',
+        'username',
+        'picture',
+        'nickname',
+        'updated_at',
+        'withdrew_at'
+    )
+    list_display_links = (
+        'user_id',
+        'picture',
+    )
+
+    def username(self, userprofile):
+        return mark_safe('{}'.format(userprofile.user.username))
+    username.short_description = '아이디'
