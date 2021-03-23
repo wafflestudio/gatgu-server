1e8c31519f3ddcf0a282afced69e5a5f0199eacd
Noaarhk <skarb8277@naver.com>
2021-03-23 14:59:44 +0900

    Add tag list

--- gatgu/article/models.py
+++ gatgu/article/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Article(models.Model):
-
     writer = models.ForeignKey(User, related_name='article', on_delete=models.CASCADE)
     title = models.CharField(max_length=50, db_index=True)
     description = models.TextField()
@@ -13,16 +12,22 @@ class Article(models.Model):
     image = models.URLField(null=True)
     people_min = models.PositiveSmallIntegerField()
     price_min = models.PositiveIntegerField()
-    time_in = models.DurationField(null=True)
-    tag = models.PositiveSmallIntegerField(null=True)
-    '''tag list to be added
-    TAG = (
-        (1, '...'),
-        (2, '...'),
-        
+
+    TAG_LIST = (
+        (1, '식품/배달음식'),
+        (2, '디지털/가구/인테리어'),
+        (3, '육아'),
+        (4, '생활/화장품'),
+        (5, '취미'),
+        (6, '의류/악세사리'),
+        (7, '반려동물용품'),
+        (8, '음반/티켓/도서'),
+        (9, '식물'),
+        (10, '기타물품'),
+
     )
-    '''
-    tag_used_count = models.PositiveIntegerField(default=0)
+
+    tag = models.PositiveSmallIntegerField(choices=TAG_LIST, null=True)
 
     written_at = models.DateTimeField(auto_now_add=True, null=False, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
