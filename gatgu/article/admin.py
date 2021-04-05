2ddfda44425e5311e684e5f1bff6eaee8daffdef
Noaarhk <skarb8277@naver.com>
2021-04-05 22:26:46 +0900

    admin start

--- gatgu/article/admin.py
+++ gatgu/article/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['writer', 'title', 'description', 'written_at', 'updated_at', 'deleted_at']
+    list_display_links = ['writer', 'title']
--- gatgu/chat/admin.py
+++ gatgu/chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import OrderChat
+
+
+@admin.register(OrderChat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ['article', 'order_status', 'tracking_number']
+    list_display_links = ['article']
