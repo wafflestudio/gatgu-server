a0c322b027efc52728f66e5167988db98845fd24
Noaarhk <skarb8277@naver.com>
2021-05-04 22:55:53 +0900

    Edit CORS

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = [
     'https://admin.gatgu.site',
     'https://dev.test.admin.gatgu.site:3000',
