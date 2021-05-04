d0b26e736261286a302445ae3ba8665ad836c972
Noaarhk <skarb8277@naver.com>
2021-05-05 00:05:15 +0900

    Edit whitelist

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -196,6 +196,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CORS_ORIGIN_ALLOW_ALL = False
+
 CORS_ORIGIN_WHITELIST = [
     'https://localhost',
     'https://localhost:3000',
