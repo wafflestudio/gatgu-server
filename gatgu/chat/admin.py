72907c34f43549483d3063fc380e7d8e3d5034a0
Namgyu Ryu <skarb8277@naver.com>
2021-05-04 14:21:51 +0900

    admin start (#96)
    * admin start
    
    * admin field custom

--- gatgu/chat/admin.py
+++ gatgu/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from chat.models import OrderChat
+
+
+# @admin.register(OrderChat)
+# class ChatAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'order_status', 'tracking_number']
