b9986e394946ee31c91ed9251937060d8f4243de
Noaarhk <skarb8277@naver.com>
2021-05-04 23:27:49 +0900

    Debug false

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'h#^&^v*zwvbqk8^8e*ne23q7e$7+ppz*dl1mp==o&do_plg9+d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DEBUG_TOOLBAR = os.getenv('DEBUG_TOOLBAR') in ('true', 'True')
 
-ALLOWED_HOSTS = ['*','0.0.0.0']
+ALLOWED_HOSTS = ['*',]
 
 # Application definition
 
