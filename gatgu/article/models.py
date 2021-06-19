cc3a2d57dfc765b51149c146f954d44cd9a9c2c0
Noaarhk <skarb8277@naver.com>
2021-06-19 19:42:34 +0900

    Put the tag model off

--- gatgu/article/models.py
+++ gatgu/article/models.py
@@ -40,39 +40,39 @@ class ArticleImage(models.Model):
     img_url = models.URLField(blank=True)
 
 
-class Tag(models.Model):
-    TAG_01 = '식품/배달음식'
-    TAG_02 = '디지털/가구/인테리어'
-    TAG_03 = '육아'
-    TAG_04 = '생활/화장품'
-    TAG_05 = '취미'
-    TAG_06 = '의류/악세사리'
-    TAG_07 = '반려동물용품'
-    TAG_08 = '음반/티켓/도서'
-    TAG_09 = '식물'
-    TAG_10 = '기타물품'
-
-    TAG_LIST = (
-        (TAG_01, '식품/배달음식'),
-        (TAG_02, '디지털/가구/인테리어'),
-        (TAG_03, '육아'),
-        (TAG_04, '생활/화장품'),
-        (TAG_05, '취미'),
-        (TAG_06, '의류/악세사리'),
-        (TAG_07, '반려동물용품'),
-        (TAG_08, '음반/티켓/도서'),
-        (TAG_09, '식물'),
-        (TAG_10, '기타물품'),
-
-    )
-    name = models.CharField(choices=TAG_LIST, blank=True, max_length=20, null=True)
-
-
-class ArticleTag(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='tags')
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tags', null=True, blank=True, max_length=50)
-
-    class Meta:
-        unique_together = (
-            ('article', 'tag')
-        )
+# class Tag(models.Model):
+#     TAG_01 = '식품/배달음식'
+#     TAG_02 = '디지털/가구/인테리어'
+#     TAG_03 = '육아'
+#     TAG_04 = '생활/화장품'
+#     TAG_05 = '취미'
+#     TAG_06 = '의류/악세사리'
+#     TAG_07 = '반려동물용품'
+#     TAG_08 = '음반/티켓/도서'
+#     TAG_09 = '식물'
+#     TAG_10 = '기타물품'
+#
+#     TAG_LIST = (
+#         (TAG_01, '식품/배달음식'),
+#         (TAG_02, '디지털/가구/인테리어'),
+#         (TAG_03, '육아'),
+#         (TAG_04, '생활/화장품'),
+#         (TAG_05, '취미'),
+#         (TAG_06, '의류/악세사리'),
+#         (TAG_07, '반려동물용품'),
+#         (TAG_08, '음반/티켓/도서'),
+#         (TAG_09, '식물'),
+#         (TAG_10, '기타물품'),
+#
+#     )
+#     name = models.CharField(choices=TAG_LIST, blank=True, max_length=20, null=True)
+#
+#
+# class ArticleTag(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='tags')
+#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tags', null=True, blank=True, max_length=50)
+#
+#     class Meta:
+#         unique_together = (
+#             ('article', 'tag')
+#         )
