2b573ec0595fbd748c2d054a808b2f30e7661b3c
Noaarhk <skarb8277@naver.com>
2021-06-24 16:19:58 +0900

    Try passing aws_credentials with gitaction

--- gatgu/user/views.py
+++ gatgu/user/views.py
@@ -433,7 +433,8 @@ class UserViewSet(viewsets.GenericViewSet):
     def get_presigned_url(self, request):
         user = request.user
         data = request.data
-        s3 = boto3.client('s3', config=Config(signature_version='s3v4', region_name='ap-northeast-2'))
+        session = boto3.Session(profile_name='default')
+        s3 = session.client('s3', config=Config(signature_version='s3v4', region_name='ap-northeast-2'))
         if data['method'] == 'get' or data['method'] == 'GET':
             url = s3.generate_presigned_url(
                 ClientMethod='put_object',
