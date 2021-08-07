58afddbe383362a34f87c31353f22758ebb719c7
Noaarhk <skarb8277@naver.com>
2021-08-07 21:41:32 +0900

    Set static files

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -258,7 +258,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = [
