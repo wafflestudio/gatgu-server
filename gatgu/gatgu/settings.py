69ed46f4576984b427b6e1c4ced4068a0e843692
Noaarhk <skarb8277@naver.com>
2021-08-30 17:47:24 +0900

    Fix settings.py

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -262,5 +262,5 @@ cred = credentials.Certificate("gatgu-firebase-admin-hs.json")
 firebase_admin.initialize_app(cred)
 
 CLIENT = boto3.client('s3', config=Config(signature_version='s3v4', region_name='ap-northeast-2'))
-BUCKET_NAME = 'gatgubucket'
+BUCKET_NAME = 'gatgu'
 MEDIA_URL = "https://%s/" % "gatgu-s3-test.s3.ap-northeast-2.amazonaws.com"
