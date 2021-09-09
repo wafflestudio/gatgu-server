139cb8d9310394d7cfd530a2718e4dd98988cb4c
Noaarhk <skarb8277@naver.com>
2021-09-09 20:07:35 +0900

    Edit layer host

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -84,8 +84,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("redis://siksha-redis-001.xevoyk.0001.apn2.cache.amazonaws.com/3", 6379)],
-            # "hosts": [("127.0.0.1", 6379)],
+            # "hosts": [("siksha-redis-001.xevoyk.0001.apn2.cache.amazonaws.com", 6379)],
+            "hosts": ["redis://siksha-redis-001.xevoyk.0001.apn2.cache.amazonaws.com:6379/3?encoding=utf-8"],
         }
     }
 }
