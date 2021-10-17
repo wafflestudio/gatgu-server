9250e78a9cb8b87df9b090d0f148a1b845fbd1d4
김기덕 <gideok@gimgideog-ui-MacBookPro-2.local>
2021-10-17 12:55:15 +0900

    change alarm

--- gatgu/push_notification/views.py
+++ gatgu/push_notification/views.py
@@ -27,10 +27,7 @@ class FCMViewSet(viewsets.GenericViewSet):
             return Response({"message: already registered"}, status=status.HTTP_200_OK)
         elif UserFCMToken.objects.filter(user=user).exists():
             user_fcm = UserFCMToken.objects.get(user=user)
-            print(user_fcm)
-            print(user_fcm.token.fcmtoken)
             tobe_deleted = FCMToken.objects.get(fcmtoken=user_fcm.token.fcmtoken)
-            print(tobe_deleted)
             tobe_deleted.delete()
         FCMToken.objects.create(fcmtoken=token)
         token_obj = FCMToken.objects.last()
