39766103fd3ba44b4b034c4d43a28cb1fc3d10fe
hiddenpunch <superkgd@gmail.com>
2022-04-07 20:52:44 +0900

    refresh repository

--- gatgu/chat/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from chat.models import OrderChat
-
-
-# @admin.register(OrderChat)
-# class ChatAdmin(admin.ModelAdmin):
-#     list_display = ['title', 'order_status', 'tracking_number']
