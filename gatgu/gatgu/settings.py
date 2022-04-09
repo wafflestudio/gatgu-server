77967e4b16a6c30773f3e5e657bef3c2eab3893d
racoon <43671073+hiddenpunch@users.noreply.github.com>
2022-04-09 17:45:37 +0900

    aws mysql rds에 django db 연결 (#256)
    * feat: install mysqlclient
    
    * feat: install django-environ
    
    * feat: database env setting

--- gatgu/gatgu/settings.py
+++ gatgu/gatgu/settings.py
@@ -9,12 +9,16 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
+import environ
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -75,10 +79,7 @@ WSGI_APPLICATION = 'gatgu.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 
